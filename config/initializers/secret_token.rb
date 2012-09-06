9e31d6e2034ee6046c2e24eb9d3b533067142197
Eyder Padoan <eyderpadoan@gmail.com>
2012-09-06 16:22:27 -0300

    Projeto padrao rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthClient::Application.config.secret_token = '16232076ae747bdfca4ff6b24fbcb61b3995cdc2b31e141529c2b4adf5e3cdefa6992e03b44065fb49a77633651c94e92f40c62788daad347b9fd15799ead55a'
